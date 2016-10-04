81e53d42aac635690e57e13f746a9fc0dab797d7
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-04 15:07:23 +0200

    starting on the random walks

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -7,22 +7,25 @@ from random import *
 def PDF(x, mean, sigma): 
 	return (1./(sigma*sqrt(2*pi))) * exp(-0.5*(x-mean)**2/sigma**2)
 
-#--------------------------------------
-#first plotting the PDF:
-
-density = linspace(-0.0002,0.0002,1000)
-
-plot(density, PDF(density, mean, sigma))
-#show()
-
-#--------------------------------------
-
-N = 1e5 #nr of random walks
-
 #first values:
 mean = 0 #defined to zero
 sigma = 0.5*1e-4 #< given in the exe.
 S_c = sqrt(sqrt(pi/sigma))
+
+#--------------------------------------
+#first plotting the PDF:
+
+density = linspace(-0.0002,0.0002,1000) # picking small density pertubations since the sigma is so small
+plot(density, PDF(density, mean, sigma))
+#show()
+
+#--------------------------------------
+#random walks
+
+
+N = 1e5 #nr of random walks
+
+
 
 delta = gauss(mean, sigma) #gauss(mu, sigma), Gaussian distribution. mu is the mean, and sigma is the standard deviation.
 #print delta
