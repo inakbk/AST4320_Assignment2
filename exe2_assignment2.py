6f54d232fa5df1a77e2a3200e9691080eaad34f6
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-04 15:04:44 +0200

    tidying

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -3,6 +3,20 @@
 from pylab import *
 from random import *
 
+#The gaussian PDF
+def PDF(x, mean, sigma): 
+	return (1./(sigma*sqrt(2*pi))) * exp(-0.5*(x-mean)**2/sigma**2)
+
+#--------------------------------------
+#first plotting the PDF:
+
+density = linspace(-0.0002,0.0002,1000)
+
+plot(density, PDF(density, mean, sigma))
+#show()
+
+#--------------------------------------
+
 N = 1e5 #nr of random walks
 
 #first values:
@@ -11,13 +25,19 @@ sigma = 0.5*1e-4 #< given in the exe.
 S_c = sqrt(sqrt(pi/sigma))
 
 delta = gauss(mean, sigma) #gauss(mu, sigma), Gaussian distribution. mu is the mean, and sigma is the standard deviation.
-print delta
+#print delta
 
 
-def PDF(x, mean, sigma):
-	return (1./(sigma*sqrt(2*pi))) * exp(-0.5*(x-mean)**2/sigma**2)
 
-density = linspace(-1,1,100)
 
-plot(density, PDF(density))
-show
+
+
+
+
+
+
+
+
+
+
+
