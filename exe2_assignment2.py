53fc769e264a7a7f6dabf622be25ffda5b064463
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-04 14:55:32 +0200

    plotting the PDF of the density

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -5,7 +5,7 @@ from random import *
 
 N = 1e5 #nr of random walks
 
-#first values
+#first values:
 mean = 0 #defined to zero
 sigma = 0.5*1e-4 #< given in the exe.
 S_c = sqrt(sqrt(pi/sigma))
@@ -13,3 +13,11 @@ S_c = sqrt(sqrt(pi/sigma))
 delta = gauss(mean, sigma) #gauss(mu, sigma), Gaussian distribution. mu is the mean, and sigma is the standard deviation.
 print delta
 
+
+def PDF(x, mean, sigma):
+	return (1./(sigma*sqrt(2*pi))) * exp(-0.5*(x-mean)**2/sigma**2)
+
+density = linspace(-1,1,100)
+
+plot(density, PDF(density))
+show
