7500f54e4be62059d70501b6fe9f888c7e774449
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-04 15:41:51 +0200

    something wrong with while loop
     or values given for epsilon or N

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -7,7 +7,7 @@ from random import *
 def PDF(x, mean, sigma): 
 	return (1./(sigma*sqrt(2*pi))) * exp(-0.5*(x-mean)**2/sigma**2)
 
-N = 1e5 #nr of random walks
+N = 100#1e5 #nr of random walks
 delta_density = zeros(N) #array to store the density values
 S_c = zeros(N)
 
@@ -16,14 +16,14 @@ mean = 0 #defined to zero
 sigma_old = 0.5*1e-4 #< given in the exe.
 S_c[0] = sqrt(sqrt(pi/sigma_old))
 delta_density[0] = gauss(mean, sigma_old) #drawing a random nr from Gaussian distribution w. a mean, and sigma is the standard deviation.
-print delta_density
+#print delta_density
 
 #--------------------------------------
 #first plotting the PDF:
 
 density = linspace(-0.0002,0.0002,1000) # picking small density pertubations since the sigma is so small
-figure(1)
-plot(density, PDF(density, mean, sigma_old))
+#figure(1)
+#plot(density, PDF(density, mean, sigma_old))
 #show()
 
 #--------------------------------------
@@ -32,19 +32,24 @@ epsilon = 1e-1 # changes S_c and will decide how fast the random walk converges
 n = 0
 
 for i in range(int(N)):
-	while S_c[i] < 1 or n < N:
+	while n < N: #S_c[i] < 1 or
 		S_c[i+1] = S_c[i] - epsilon
 		sigma_new = pi/S_c[i+1]**4 
 		beta = gauss(mean, sqrt(sigma_new**2 - sigma_old**2))
+		print beta
 		delta_density[i+1] = delta_density[i] + beta
 
 		sigma_old = sigma_new #updating sigma
+		print n
 		n += 1
 
-
+print delta_density
+print S_c
 
 figure(2)
-plot(delta_density[0:n+1], S_c[0:n+1], 'ro-')
+plot(S_c[0:n+1], delta_density[0:n+1], 'ro-')
+xlabel('S_c')
+ylabel('delta_density')
 show()
 
 
