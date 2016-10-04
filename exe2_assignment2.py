dedddce39296eef9b5a2db09bf74570a48195658
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-04 18:21:39 +0200

    looks like random walk (10 walkers) but
    noone hits delta_crit….

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -12,14 +12,15 @@ N = 10#1e5 #nr of random walks
 #--------------------------------------
 #random walks
 epsilon = 0.5 # changes S_c and will decide how fast the random walk converges
-realization_time = 0
 maximum_number_of_iterations = 500
 
-delta_density = zeros(maximum_number_of_iterations) #array to store the density values
-S_c = zeros(maximum_number_of_iterations)
 final_delta_density = zeros(N) #storing the density values after realization to make histogram
 
 for k in range(N-1):
+	delta_density = zeros(maximum_number_of_iterations) #array to store the density values to plot random walk
+	S_c = zeros(maximum_number_of_iterations)
+	realization_time = 0
+
 	#first values:
 	mean = 0 #defined to zero
 	sigma_old = 0.5*1e-4 #< given in the exe.
@@ -29,29 +30,33 @@ for k in range(N-1):
 	for i in range(maximum_number_of_iterations):
 		S_c[i+1] = S_c[i] - epsilon
 		sigma_new = pi/S_c[i+1]**4 
-		print S_c[i]
+		#print S_c[i]
 		beta = gauss(mean, sqrt(sigma_new**2 - sigma_old**2))
 		delta_density[i+1] = delta_density[i] + beta
 
 		sigma_old = sigma_new #updating sigma
-		print realization_time
+		#print realization_time
 		realization_time += 1
 
 		if S_c[i] < 1:
 			print "Realization happened! realization_time= ", realization_time
 			print "S_c= ", S_c[i]
 			final_delta_density[k] = delta_density[i+1]
+			print "density= ", final_delta_density[k]
+			#print "density_next= ", delta_density[i+2]
 			break
 		if realization_time > maximum_number_of_iterations:
 			print "Maximum nr of iterations reached!!! (", maximum_number_of_iterations, ")"
 			final_delta_density[k] = delta_density[i+1]
+			print "density= ", final_delta_density[k]
 			break
 
 #	figure(1)
-	plot(S_c[0:realization_time], delta_density[0:realization_time])
-	plot(S_c[0], delta_density[0], 'bo-') #plotting first point blue
+	plot(S_c[0:realization_time-10], delta_density[0:realization_time-10])
+	#print delta_density[0:realization_time]
+	#plot(S_c[0], delta_density[0], 'bo-') #plotting first point blue
 
-plot(S_c[0:realization_time+1], 1 + zeros(realization_time+1), 'g')
+plot(S_c[0:realization_time], 1 + zeros(realization_time), 'ro')
 xlabel('S_c')
 ylabel('delta_density')
 #axis([0.5,S_c[0],-5,5])
