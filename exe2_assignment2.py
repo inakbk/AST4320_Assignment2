7b1a6489e6bec7e00fe89bb915b5dfd3bab7ed14
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-04 15:12:50 +0200

    exe 2.1 done

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -7,10 +7,18 @@ from random import *
 def PDF(x, mean, sigma): 
 	return (1./(sigma*sqrt(2*pi))) * exp(-0.5*(x-mean)**2/sigma**2)
 
+N = 1e5 #nr of random walks
+delta_density = zeros(N) #array to store the density values
+S_c = zeros(N)
+
 #first values:
 mean = 0 #defined to zero
 sigma = 0.5*1e-4 #< given in the exe.
-S_c = sqrt(sqrt(pi/sigma))
+S_c[0] = sqrt(sqrt(pi/sigma))
+delta_density[0] = gauss(mean, sigma) #drawing a random nr from Gaussian distribution w. a mean, and sigma is the standard deviation.
+#print delta
+
+
 
 #--------------------------------------
 #first plotting the PDF:
@@ -23,12 +31,10 @@ plot(density, PDF(density, mean, sigma))
 #random walks
 
 
-N = 1e5 #nr of random walks
 
 
 
-delta = gauss(mean, sigma) #gauss(mu, sigma), Gaussian distribution. mu is the mean, and sigma is the standard deviation.
-#print delta
+
 
 
 
