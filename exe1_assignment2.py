c9038b043b96226108147011a539499947b17543
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-05 13:34:42 +0200

    doing my own FWHM

--- exe1_assignment2.py
+++ exe1_assignment2.py
@@ -11,17 +11,27 @@ def W(k):
 
 k = linspace(-10,10,N)
 
-W_max = max(W(k))
-W_min = min(W(k))
-
 #finding the FWHM
 k2 = linspace(-1,1,N)
 r = UnivariateSpline(k2, W(k2))
-r1, r2 = r.roots()/2.
+print r.roots()
+
+
+
+r1, r2 = r.roots()/2. 
+
+W_max = max(W(k2))
+HM = W_max/2 #half maximum
+
+for i in range(N):
+	if W(k[i]) > HM:
+		print i
+		r1, r2 = k[i]
+		break
 
 plot(k, W(k))
-plot(r1, W(r1), 'ro')
-plot(r2, W(r2), 'ro')
+plot(r1, HM, 'ro')
+plot(r2, HM, 'ro')
 xlabel('$k$', fontsize=22)
 ylabel('$W^*(k)$', fontsize=22)
 title('The fourier transform of the smoothing function W(k) in 1D')
