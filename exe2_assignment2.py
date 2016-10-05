97ea47112633d5f8bf6e928b11dacf00c77dff6e
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-05 10:51:22 +0200

    playing with parameters
    looks like smaller epsilon makes the histogram resemble the PDF more

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -16,8 +16,8 @@ def PDF_nc(x, mean, sigma):
 
 N = 1000 #1e5 nr of random walks! :)
 
-epsilon = 0.01 # changes S_c and will decide how fast the random walk converges
-maximum_number_of_iterations = 500
+epsilon = 0.02 # changes S_c and will decide how fast the random walk converges
+maximum_number_of_iterations = 1000
 
 final_delta_density = zeros(N) #storing the density values after realization to make histogram
 threshold_delta_density = []
