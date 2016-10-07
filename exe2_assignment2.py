7bbef9bde2e4ed370f1ac0af51db2a3a7e0e7ed0
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2016-10-07 10:34:53 +0200

    small plot fix

--- exe2_assignment2.py
+++ exe2_assignment2.py
@@ -93,7 +93,7 @@ hist(final_delta_density, normed=1, bins=300)
 
 density_PDF = linspace(-12, 2, 1000) 
 sigma_PDF = sqrt(pi) # since S_c=1 at the end of the chain
-plot(density_PDF, PDF(density_PDF, mean, sigma_PDF), 'r')
+plot(density_PDF, PDF(density_PDF, mean, sigma_PDF), 'r', linewidth=3)
 
 #--------------------------------------
 #plotting the normalized histogram and PDF for all densities under delta_crit:
